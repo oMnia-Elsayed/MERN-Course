281604def05e74b69c932d09622153eec6164272
oMnia Elsayed <omnia.gnena@vodafone.com>
2021-10-19 16:57:27 +0200

    add passport-google-oauth

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 
+// for authentication from passport-google-oauth20
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
+/**
+ * new GoogleStrategy() -> create new instance of google-strategy
+ * to authenticate my users with google
+ * passport.use -> to authenticate in general but don't know how to authenticate
+ * users with a very specific provider like google
+ * there is a new strategy to use, user can use to authenticate themselves inside application
+ */
+passport.use(new GoogleStrategy());
 
-app.get('/', (req, res) => {
-    res.send({ Hi: 'Hi'});
-});
-
-// process.env.PORT => if there is an evn variable defined by heroku or use 5000
+// process.env.PORT -> if there is an evn variable defined by heroku or use 5000
 const PORT = process.env.PORT || 5000; 
 
 app.listen(PORT);
